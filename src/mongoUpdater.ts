eb93bdbae5cdc528af870163dec5ee1045df5f9f
anon <joshrwalter@gmail.com>
2024-05-23 16:30:40 +1000

    push changes

--- src/mongoUpdater.ts
+++ src/mongoUpdater.ts
@@ -9,90 +9,75 @@ import { logError, logInfo, logSuccess } from "./log";
 // Database Name
 const dbName = "reaction";
 
-async function main() {
-  // Create a new MongoClient
+// Create a new MongoClient
 
-  logInfo("Using mongo url: " + __MONGO_CONN);
-  const client = new MongoClient(__MONGO_CONN);
+logInfo("Using mongo url: " + __MONGO_CONN);
+const client = new MongoClient(__MONGO_CONN);
 
-  try {
-    // Connect to the MongoDB cluster
-    await client.connect();
+// Connect to the MongoDB cluster
+client.connect().then(() => {
+  logInfo(`Connected successfully to mongo server`);
 
-    logInfo(`Connected successfully to mongo server`);
+  const db: Db = client.db(dbName);
+  const catalogCollection = db.collection("Catalog");
+  const productsCollection = db.collection("Product");
+  const stream = createReadStream(__OUT_CSV_FILE);
 
-    const db: Db = client.db(dbName);
-    const catalogCollection = db.collection("Catalog");
-    const productsCollection = db.collection("Product");
-    const stream = createReadStream(__OUT_CSV_FILE);
+  const pipeFn = () => {
+    const pipe = stream.pipe(csvParser());
 
-    const pipeFn = (): Promise<void> => {
-      return new Promise((resolve, reject) => {
-        const pipe = stream.pipe(csvParser());
-        let isLastDataEventProcessed = false;
+    pipe.on("data", async (data) => {
+      pipe.pause();
 
-        pipe.on("data", async (data) => {
-          pipe.pause();
+      const description = data["product_description"];
 
-          const description = data["product_description"];
+      logInfo("Updated catalog: " + data["catalog_id"]);
+      await catalogCollection.updateOne(
+        // @ts-ignore
+        { _id: data["catalog_id"] },
+        {
+          $set: {
+            "product.description": description,
+          },
+        }
+      );
 
-          logInfo("Updated catalog: " + data["catalog_id"]);
-          await catalogCollection.updateOne(
-            // @ts-ignore
-            { _id: data["catalog_id"] },
-            {
-              $set: {
-                "product.description": description,
-              },
-            }
-          );
+      logSuccess("Updated catalog: " + data["catalog_id"]);
 
-          logSuccess("Updated catalog: " + data["catalog_id"]);
+      logInfo("Updating product: " + data["product_id"]);
 
-          logInfo("Updating product: " + data["product_id"]);
+      await productsCollection.updateOne(
+        // @ts-ignore
+        { _id: data["product_id"] },
+        {
+          $set: {
+            description: description,
+          },
+        }
+      );
 
-          await productsCollection.updateOne(
-            // @ts-ignore
-            { _id: data["product_id"] },
-            {
-              $set: {
-                description: description,
-              },
-            }
-          );
+      logSuccess("Updated product: " + data["product_id"]);
 
-          logSuccess("Updated product: " + data["product_id"]);
+      pipe.resume();
+    });
 
-          pipe.resume();
+    pipe.on("end", () => {
+      console.log("end");
+    });
 
-          if (isLastDataEventProcessed) {
-            logSuccess("Finished updating the mongo db!");
-            resolve();
-          }
-        });
+    pipe.on("close", () => {
+      client.close();
+      console.log("close!");
+    });
 
-        pipe.on("end", () => {
-          // Only resolve the promise if this is the last data event processed
-          isLastDataEventProcessed = true;
-        });
+    pipe.on("error", (err) => {
+      console.log(err, "err");
+    });
+  };
 
-        pipe.on("error", (err) => {
-          console.log(err, "err");
-          reject(err);
-        });
-      });
-    };
+  pipeFn();
 
-    await pipeFn();
+  // Example update operation
 
-    // Example update operation
-  } catch (err) {
-    logError("Error: ", err);
-    console.error(err);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-
-main().catch(console.error);
+  // Ensures that the client will close when you finish/error
+});
